e9e8f1ac47b9b3ba7082d56167bcd42c3dad7cd7
Asmoddey <112192202+Asmoddey@users.noreply.github.com>
2022-12-15 22:57:53 +0200

    Add files via upload

--- go_to_api/api_01.py
+++ go_to_api/api_01.py
@@ -1,19 +1,21 @@
 import requests
 server = "https://qauto.forstudy.space/api"
-endpoint = "/auth/signin"
-# сервер та ендпоінт задають урл який ми тестимо
-# далі данні що ми шлемо методом пост
+endpoint_auth = "/auth/signin"
+endpoint_users = "/users/current"
+  # сервер та ендпоінт задають урл який ми тестимо
+  # далі данні що ми шлемо методом пост
 example_value = {
-  "email": "test@test.com",
-  "password": "Qwerty12345",
+  "email": "qam2608@2022test.com",
+  "password": "Qam2608venv",
   "remember": False
 }
-# викликаємо метод пост і json= вказує що ми шлем як ДЖСОН
-r = requests.post(server+endpoint, json=example_value)
-# сервер присилає нм теж ДЖСОН що ми перетворюємол в словник і записуєм у зм. 
+  # викликаємо метод пост і json= вказує що ми шлем як ДЖСОН
+ses = requests.session()
+r = ses.post(server+endpoint_auth, json=example_value)
+  # сервер присилає нм теж ДЖСОН що ми перетворюємол в словник і записуєм у зм. 
 server_says = r.json()
-# використовуємо методи словника
-#  Якщо ключ "status" у словнику то
+  # використовуємо методи словника
+  #  Якщо ключ "status" у словнику то
 if "status" in server_says:
     #  Якщо значення клюса статус == ок
     if server_says["status"] == "ok":
@@ -21,3 +23,10 @@ if "status" in server_says:
     #  інакше
     else:
         print(server_says['message'])
+get_code = ses.get(server+endpoint_users)
+#get_code = gr.json()
+if get_code.status_code == 200:
+    print("Статус код GET-запиту: ", get_code.status_code)
+    print("Результат GET-запиту: ", get_code.json())
+else:
+    print("Потрібна авторизація: ", get_code.json())
